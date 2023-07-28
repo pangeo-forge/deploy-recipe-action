27efcb49c640b2f212ff232bb46f73d6b950b783
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-07-28 15:46:30 -0700

    test realism refinements

--- action/test_main.py
+++ action/test_main.py
@@ -1,27 +1,44 @@
 import os
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from deploy_recipe import main
 
 
 @patch("deploy_recipe.open")
 @patch("deploy_recipe.os.listdir")
-@patch("deploy_recipe.subprocess")
+@patch("deploy_recipe.subprocess.run")
 @patch("deploy_recipe.requests")
 @patch.dict(
     os.environ,
     {
-        "CONDA_ENV": "0",
-        "GITHUB_REPOSITORY": "1",
-        "GITHUB_SERVER_URL": "2",
-        "GITHUB_API_URL": "3",
-        "GITHUB_HEAD_REF": "4",
-        "GITHUB_REPOSITORY_ID": "5",
-        "GITHUB_RUN_ID": "6",
-        "GITHUB_RUN_ATTEMPT": "7",
-        "INPUT_PANGEO_FORGE_RUNNER_CONFIG": "8",
-        "INPUT_SELECT_RECIPE_BY_LABEL": "9",
+        "CONDA_ENV": "notebook",
+        "GITHUB_REPOSITORY": "my/repo",
+        "GITHUB_SERVER_URL": "https://github.com",
+        "GITHUB_API_URL": "https://api.github.com",
+        "GITHUB_HEAD_REF": "abcdefg",
+        "GITHUB_REPOSITORY_ID": "1234567890",
+        "GITHUB_RUN_ID": "0987654321",
+        "GITHUB_RUN_ATTEMPT": "1",
+        "INPUT_PANGEO_FORGE_RUNNER_CONFIG": '{"a": "b"}',
+        "INPUT_SELECT_RECIPE_BY_LABEL": "",
     },
 )
-def test_main(open, listdir, subprocess, requests):
+def test_main(
+    requests: MagicMock,
+    subprocess_run: MagicMock,
+    listdir: MagicMock,
+    open: MagicMock,
+):
+    class MockProcess:
+        stdout = b'{"job_id": "foo", "job_name": "bar"}'
+        stderr = b"456"
+        returncode = 0
+
+    subprocess_run.return_value = MockProcess()
     main()
+    # open is only called if listdir('feedstock') contains requirements.txt
+    # open.assert_called_once()
+    listdir.assert_called_once()
+    subprocess_run.assert_called()
+    # requests is only called if INPUT_SELECT_RECIPE_BY_LABEL=true
+    # requests.assert_called()
